cd942f3ec9c628504241f9716a190e077ef101c5
Nihar Hegde <niharhegde163@gmail.com>
2024-03-13 11:12:22 +0530

    lates commit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.ui.alpha" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
